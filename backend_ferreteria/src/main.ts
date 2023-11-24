8710fbc464e98a3f6a44079363233207f56db085
Ruben Arteaga <rubentec3@gmail.com>
2023-11-23 22:30:24 -0400

    autorizar usuario

--- backend_ferreteria/src/main.ts
+++ backend_ferreteria/src/main.ts
@@ -5,21 +5,28 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
 
   const config = new DocumentBuilder()
-    .setTitle('API Rest FERRETERIA')
+    .setTitle('SIS257')
     .setDescription('API de la materia Desarrollo Aplicación INT/Internet II')
     .setVersion('1.0')
-    .addTag('producto, categoria, unidad, detalle, venta')
+    .addTag('-----')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('apidoc', app, document);
 
   await app.listen(process.env.PORT);
-  console.log(`API corriendo en ${await app.getUrl()}/apidoc`);
+  console.log(`Api corriendo en ${await app.getUrl()}/apidoc`);
 }
 bootstrap();
